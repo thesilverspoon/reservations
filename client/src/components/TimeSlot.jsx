6b6db16c10a501900a26612e601dc0ce4f3da645
tmkuba <tmk.rss@gmail.com>
2018-02-26 11:16:11 -0800

    Basic working TimeSlot component

--- client/src/components/TimeSlot.jsx
+++ client/src/components/TimeSlot.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const TimeSlot = () => <div>TimeSlot</div>;
+const TimeSlot = props => (
+  <div>
+    <button
+      disabled={!props.available}
+      onClick={() => props.clickHandler(props.time)}
+    >
+      {props.time}:00 PM
+    </button>
+  </div>
+);
+
+
+TimeSlot.propTypes = {
+  time: PropTypes.number.isRequired,
+  available: PropTypes.bool.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default TimeSlot;
